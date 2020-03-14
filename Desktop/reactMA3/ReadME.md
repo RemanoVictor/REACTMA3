dbffdcc80970fc21c02c9a521b4bb5da56bbe200
Remano <remanovictor@icloud.coms>
2020-03-15 00:38:22 +0100

    styling added to assignment

---
+++ Desktop/reactMA3/ReadME.md
@@ -0,0 +1,3 @@
+Username: Victor
+
+Password: cameron 
---
+++ Desktop/reactMA3/client/src/components/cards.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Cards = props => {
+  return (
+    <div className="[ col-sm-12 ] [ card ]">
+      <h3> {props.name} </h3>
+      <img src={props.img} alt="just random" className="[ CardImg ]" />
+      <p> {props.gender} </p>
+      <Link to={`/CardSpecific/${props.id}`} className="btn btn-primary">
+        Read More
+      </Link>
+    </div>
+  );
+};
+
+export default Cards;
