17634f9f3a2b594792ee683b00391eae1522e287
Remano <remanovictor@icloud.coms>
2020-04-22 21:49:41 +0200

    moved files

--- client/src/constants/constant.js
+++
@@ -1,5 +0,0 @@
-//Rck and Morty API
-export const RICK_API = "https://rickandmortyapi.com/api/character/";
-
-// Bypass CORS
-export const HEROKU_BYPASS_CORS = "https://cors-anywhere.herokuapp.com/";
